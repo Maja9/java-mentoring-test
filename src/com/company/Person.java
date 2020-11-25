6ced66d2bb21f110a384332a2596be438e66681d
Maja Domin <majadomin@gmail.com>
2020-11-25 21:40:43 +0100

    edit person

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -19,5 +19,15 @@ public class Person {
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
